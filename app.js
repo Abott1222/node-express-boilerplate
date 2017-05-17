af16b8193803f7bbd5f9243c5d201471f97b9cfc
rogerwschmidt <rogerschmidt@gmail.com>
2017-05-17 09:54:24 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', function(req,res,next){
+  res.json({message:"This is a message from the api"});
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  var error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.sendStatus(err.status || 500).send(error);
+
+});
+
+module.exports = app;
